caa4abf47f3fec50afcc16b5a068e91f20c4ec3a
Marysia Pabich <marysia.pabich@gmail.com>
2019-12-03 21:31:54 +0100

    Prepare new components for functionality to submit a form

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import OrderSummary from '../../features/OrderSummary/OrderSummary';
+import { Row, Col} from 'react-flexbox-grid';
+
+
+
+const OrderForm = () => (
+
+  <Row>
+    <Col xs={12}>
+      <OrderSummary />
+    </Col>
+  </Row>
+
+);
+
+export default OrderForm;
