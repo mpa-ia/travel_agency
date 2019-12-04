9eb1a06d128178de6d37fe5a55d67f094c11588e
Marysia Pabich <marysia.pabich@gmail.com>
2019-12-04 18:16:33 +0100

    Prepare access to order options

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,19 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import OrderSummary from '../../features/OrderSummary/OrderSummary';
+import OrderOption from '../../features/OrderOption/OrderOption';
 import { Row, Col} from 'react-flexbox-grid';
+import pricing from '../../../data/pricing.json';
 
 
 
-const OrderForm = props => (
+const OrderForm = props => {
+  return (
+    <Row>
+      {pricing.map(option => (
+        <Col key={option.id} md={4}>
+          <OrderOption {...option} />
+        </Col>
+      )
+      )}
+      <Col xs={12}>
+        <OrderSummary cost={props.tripCost} options={props.options} />
+      </Col>
+    </Row>
+  );
 
-  <Row>
-    <Col xs={12}>
-      <OrderSummary cost={props.tripCost} options={props.options}/>
-    </Col>
-  </Row>
-
-);
+};
 
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
