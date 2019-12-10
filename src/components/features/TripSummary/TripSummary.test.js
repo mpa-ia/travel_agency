d7a0048386b865f9a2bd6a2a6e8d798086598e16
Marysia Pabich <marysia.pabich@gmail.com>
2019-12-10 22:38:46 +0100

    Unit test - check whether component renders correct img & alt

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -8,4 +8,13 @@ describe('Component TripSummary', () => {
     const component = shallow(<TripSummary id={id} image='image.jpg' name='name' cost='1000' tags={['a', 'b']}/>);
     expect(component.find('.link').prop('to')).toEqual(`/trip/${id}`);
   });
+
+  it('renders correct img & alt', () => {
+    const expectedImg = 'image.jpg';
+    const expectedAlt = 'abc';
+    const component = shallow(<TripSummary image={expectedImg} id='id' name={expectedAlt} cost='1000' tags={['a', 'b']} />);
+
+    expect(component.find('img').prop('src')).toEqual(expectedImg);
+    expect(component.find('img').prop('alt')).toEqual(expectedAlt);
+  });
 });
