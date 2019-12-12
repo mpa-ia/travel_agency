fbb8e1a0cfebbe4cae8105b6f6d22ecdeccd87fa
Marysia Pabich <marysia.pabich@gmail.com>
2019-12-12 20:11:25 +0100

    Fix bug - update state with chosen date

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,20 +1,30 @@
 import React, {useState} from 'react';
 import DatePicker from 'react-datepicker';
+import PropTypes from 'prop-types';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
 // CSS Modules, react-datepicker-cssmodules.css
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-const OrderOptionDate = () => {
+const OrderOptionDate = ({ setOptionValue }) => {
+
   const [startDate, setStartDate] = useState(new Date());
+
   return (
     <DatePicker
       selected={startDate}
-      onChange={date => setStartDate(date)}
+      onChange={date => {
+        setStartDate(date);
+        setOptionValue(date);
+      }}
       minDate={new Date()}
     />
   );
+};
+
+OrderOptionDate.propTypes = {
+  setOptionValue: PropTypes.func,
 };
 
 export default OrderOptionDate;
