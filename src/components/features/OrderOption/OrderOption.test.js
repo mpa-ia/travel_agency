83ac8f01d6588eb6c03cf283a2fde6aed7857d6e
Marysia Pabich <marysia.pabich@gmail.com>
2019-12-13 22:53:25 +0100

    Test whether comp. runs setOrderOption on change

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -58,7 +58,7 @@ const mockPropsForType = {
   date: {},
 };
 
-// const testValue = mockProps.values[1].id;
+const testValue = mockProps.values[1].id;
 // const testValueNumber = 3;
 
 for (let type in optionTypes) {
@@ -66,11 +66,14 @@ for (let type in optionTypes) {
     let component;
     let subcomponent;
     let renderedSubcomponent;
+    let mockSetOrderOption;
 
     beforeEach(() => {
+      mockSetOrderOption = jest.fn();
       component = shallow(
         <OrderOption
           type={type}
+          setOrderOption={mockSetOrderOption}
           {...mockProps}
           {...mockPropsForType[type]}
         />
@@ -96,6 +99,12 @@ for (let type in optionTypes) {
           expect(options.length).toBe(mockProps.values.length);
           expect(options.at(0).prop('value')).toBe(mockProps.values[0].id);
           expect(options.at(1).prop('value')).toBe(mockProps.values[1].id);
+        });
+
+        it('should run SetOrderOption function on change', () => {
+          renderedSubcomponent.find('select').simulate('change', {currentTarget: {value: testValue}});
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({[mockProps.id]: testValue});
         });
         break;
       }
