13dddcd22901a16fddc7cb67c87b8bc01507fd75
Marysia Pabich <marysia.pabich@gmail.com>
2019-12-15 15:05:07 +0100

    Install json server, add functionality to send form to API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,9 +3,41 @@ import PropTypes from 'prop-types';
 
 import OrderSummary from '../../features/OrderSummary/OrderSummary';
 import OrderOption from '../../features/OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
 import { Row, Col} from 'react-flexbox-grid';
-import pricing from '../../../data/pricing.json';
 
+import pricing from '../../../data/pricing.json';
+import settings from '../../../data/settings.js';
+
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse) {
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 const OrderForm = ({options, tripCost, setOrderOption}) => {
   return (
     <Row>
@@ -17,6 +49,7 @@ const OrderForm = ({options, tripCost, setOrderOption}) => {
       )}
       <Col xs={12}>
         <OrderSummary cost={tripCost} options={options} />
+        <Button onClick={() => sendOrder(options, tripCost)}>Order Now!</Button>
       </Col>
     </Row>
   );
