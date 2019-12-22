78afdda095de0c0647e611313eed865865176d64
Marysia Pabich <marysia.pabich@gmail.com>
2019-12-22 11:52:35 +0100

    Prepare title & description for HappyHourAd

--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -2,9 +2,21 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
+const select = {
+  title: '.title',
+  description: '.promoDescription',
+};
+
 describe('Component HappyHourAdd', () => {
   it('should render without crashing', () => {
     const component = shallow(<HappyHourAd />);
     expect(component).toBeTruthy();
+  });
+
+  it('should render header and description', () => {
+    const component = shallow(<HappyHourAd />);
+
+    expect(component.exists(select.title)).toEqual(true);
+    expect(component.exists(select.description)).toEqual(true);
   });
 });
