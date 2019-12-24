878d3086eb92fcf27fc5619135d9f32d996d8325
Marysia Pabich <marysia.pabich@gmail.com>
2019-12-24 09:51:04 +0100

    Return time in hh:mm:ss format

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -2,6 +2,10 @@ export const formatTime = seconds => {
 
   if (seconds == null || isNaN(seconds) || seconds < 0) {
     return null;
+  } else {
+    let date = new Date(null);
+    date.setSeconds(seconds);
+    const timeString = date.toISOString().substr(11, 8);
+    return timeString;
   }
-
 };
