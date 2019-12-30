072df49f82df477a1d44e81bcd1c70d35f369981
Marysia Pabich <marysia.pabich@gmail.com>
2019-12-30 08:16:23 +0100

    Add special price to trip summary panel

--- src/components/features/TripSummary/TripSummary.js
+++ src/components/features/TripSummary/TripSummary.js
@@ -4,6 +4,9 @@ import {Link} from 'react-router-dom';
 import styles from './TripSummary.scss';
 import {Col} from 'react-flexbox-grid';
 
+import { discountPrice } from '../../../utils/discountPrice';
+import { formatPrice } from '../../../utils/formatPrice';
+
 const TripSummary = ({id, image, name, cost, days, tags}) => (
   <Col xs={12} sm={6} lg={4} className={styles.column}>
     <Link to={`/trip/${id}`} className={styles.link}>
@@ -12,7 +15,8 @@ const TripSummary = ({id, image, name, cost, days, tags}) => (
         <h3 className={styles.title}>{name}</h3>
         <div className={styles.details}>
           <span>{days} days</span>
-          <span>from {cost}</span>
+          <span>SPECIAL PRICE from {formatPrice(discountPrice(cost, 20))}</span>
+          <span><small>Standard price from {cost}</small></span>
         </div>
         {tags == null ? null : (
           <div className={styles.tags}>
